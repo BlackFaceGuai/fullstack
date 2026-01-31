214835b24c9becd7ef8ef30ff0e433f5f08b095c
BlackFace <zzyouxiang@gmail.com>
2026-01-31 23:58:32 +0800

    part2.4-going

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,43 +1,67 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => {
+        return <Course key={course.id} course={course}/>
+      })}
     </div>
   )
 }
 
-const Course = ({course}) => {
-  console.log(course);
-  
+const Course = (props) => {
+  const course = props.course;
+  console.log("course:", course);
+  const parts = course.parts;
   return (
     <>
+      {parts.map(part => {
+        return <Part key={part.id} part={part.name} exercises={part.exercises} />
+      })}
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
