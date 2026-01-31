b766b0436ea6460c44aa38a2b0346f3f8da0e9de
BlackFace <zzyouxiang@gmail.com>
2026-01-31 14:36:17 +0800

    练习2.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -17,6 +17,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
@@ -51,6 +56,7 @@ const Content = (props) => {
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
       <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part part={props.parts[3].name} exercises={props.parts[3].exercises} />
     </>
   )
 }
@@ -66,7 +72,8 @@ const Part = ({part, exercises}) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      <p style={{ fontWeight: 'bold' }}>total of {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises + props.parts[3].exercises} exercises</p>
+      {/* <p style={{ fontWeight: 'bold' }}>total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</p> */}
     </>
   )
 }
