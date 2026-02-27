512e277310cbcad4e472c9c2a3324a8d072dee87
BlackFace <zzyouxiang@gmail.com>
2026-02-27 23:05:26 +0800

    part3b部署应用到互联网上

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -100,7 +100,7 @@ app.delete('/api/notes/:id', (request, response) => {
 app.use(unknownEndpoint)
 
 // 启动服务器
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
